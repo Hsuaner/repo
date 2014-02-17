016c552c06907013842691db070428fd32e2d455
Hsuaner <tiffany444222555@hotmail.com>
2014-02-18 00:48:47 +0800

    repo
    WEEK1

---
+++ week1.java
@@ -0,0 +1,10 @@
+public class week1 {
+  public static void main(String args[])  { 
+  
+	System.out.println("回鄉偶書 -- 唐．賀知章\n\n");
+	System.out.println("少小離家老大回，\n");
+	System.out.println("鄉音無改鬢毛摧。\n");
+	System.out.println("兒童相見不相識，\n");
+	System.out.println("笑問客從何處來。\n");
+  }
+}
